2853079d8382a3db27d0b8b02c49a11a9e21bcf6
adrien <adrien.clausel@live.fr>
2026-03-23 18:58:55 +0100

    Sprint 1: update gateway

---
+++ gateway/src/main/java/com/medilabo/gateway/filter/AuthHeaderFilter.java
@@ -0,0 +1,30 @@
+package com.medilabo.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthHeaderFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String userName = exchange.getRequest().getHeaders().getFirst("X-User-Name");
+
+        if (userName == null || userName.isBlank()) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -100;
+    }
+}
